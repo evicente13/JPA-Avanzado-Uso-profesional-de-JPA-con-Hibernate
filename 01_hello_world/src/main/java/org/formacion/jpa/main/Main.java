9e61dc51600fbc250079d105bbf1dbb5ccf44b77
Eduardo Vicente Marroquín <eduardoartemis@gmail.com>
2023-02-14 23:00:41 -0500

    Video 10 - 14/02/2023 23:00

--- 01_hello_world/src/main/java/org/formacion/jpa/main/Main.java
+++ 01_hello_world/src/main/java/org/formacion/jpa/main/Main.java
@@ -1,34 +1,61 @@
 package org.formacion.jpa.main;
 
-import org.formacion.jpa.domain.Persona;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.spi.PersistenceProvider;
+import javax.persistence.spi.PersistenceUnitInfo;
+
+import org.formacion.jpa.domain.Persona;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_main");
+		/*
+		 * Forma "tradicional" de crear un entity manager factory
+		 * 
+		 * EntityManagerFactory emf =
+		 * Persistence.createEntityManagerFactory("jpa_main");
+		 * 
+		 * A partir de este punto se simula el comportamiento de un contenedor
+		 */
 
-        EntityManager em = emf.createEntityManager();
+		// La clase HibernatePersistenceProvider es la que especificamos
+		// en el fichero persistente.xml en el elemento <provider>
+		PersistenceProvider hpp = new HibernatePersistenceProvider();
 
-        em.getTransaction().begin();
+		PersistenceUnitInfo persistenceInfo = new SimplePersistenceInfo("no-main");
 
-        Persona nueva = new Persona();
-        nueva.setNombre("pepe");
+		// Propiedades generales
+		Map<String, String> properties = new HashMap<>();
 
-        em.persist(nueva);
+		properties.put("javax.persistence.jdbc.url", "jdbc:h2:mem:test");
+		properties.put("javax.persistence.schema-generation.database.action", "create");
+		properties.put("hibernate.show_sql", "true");
 
-        em.getTransaction().commit();
+		// Ya tenemos los dos ingredientes para crear el EntityManagerFactory
+		EntityManagerFactory emf = hpp.createContainerEntityManagerFactory(persistenceInfo, properties);
 
-        em.close();
-        
-        // Cerramos el entity manager factory para provocar que se cierre el pool
-        // de conexiones que crea Hibernate y no deje la apliacion sin finalizar.
-        emf.close();
+		// A partir de este punto el código es idéntico a los anteriores proyectos
+		EntityManager em = emf.createEntityManager();
 
-    }
+		em.getTransaction().begin();
+
+		Persona nueva = new Persona();
+		nueva.setNombre("pepe");
+
+		em.persist(nueva);
+
+		em.getTransaction().commit();
+
+		em.close();
+
+		emf.close();
+
+	}
 
 }
